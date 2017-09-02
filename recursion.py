a870d8ed19cfe0f79949a70c8396fd821a091e59
Jasmine Jackson <Jasmine@Jasmine-Jacksons-MacBook-Pro.local>
2017-09-01 17:23:47 -0700

    Flatten nested list

--- recursion.py
+++ recursion.py
@@ -16,13 +16,33 @@ class Node:
 def is_valid(node, mini=None, maxi=None):
     """Is this tree a valid BST?"""
 
+    # if we reach None (the end of the tree), we know all conditions passed.
     if node is None:
         return True
 
+    # the left children can't be greater than the parent (maximum number)
     if maxi is not None and node.data > maxi:
         return False
 
+    # the right children can't be less than the parent (minimum number)
     if mini is not None and node.data < mini:
         return False
     
+    # recurse on the left and right sides
     return is_valid(node.left, mini, node.data) and is_valid(node.right, node.data, maxi)
+
+
+####################
+### Flatten list ###
+####################
+
+def flatten(lst):
+    """Given a nested list, returns a flattened list"""
+
+    if lst == []:
+        return lst
+
+    if type(lst[0]) == list:
+        return flatten(lst[0]) + flatten(lst[1:])
+
+    return lst[:1] + flatten(lst[1:])
